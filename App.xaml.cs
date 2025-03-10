283ee57b38dbd512ce069445d81c80e8d01b36d0
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-03-10 15:25:39 +0530

    splash screen updated

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,5 +9,16 @@ namespace StructViz3D;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // Do NOT create MainWindow at all at this point
+        this.MainWindow = null;  // Explicitly set to null
+
+        // Only create and show the splash screen
+        var splashScreen = new SplashScreen();
+        splashScreen.Show();
+    }
 }
 
